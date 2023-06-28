b2ebd3054df6b98c9c163886e1610238b8b58aae
Dora Do <dora.thien.do@gmail.com>
2023-06-28 01:33:36 -0400

    add multiple pages to webpack

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -3,25 +3,43 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
+const pages = ["index", "projects"];
+
 module.exports = {
-    entry: path.resolve(__dirname, '../src/script.js'),
-    output:
-    {
-        filename: 'bundle.[contenthash].js',
+    entry: pages.reduce((config, page) => {
+        config[page] = `./src/${page}.js`;
+        return config;
+    }, {}),
+    output: {
+        filename: '[name].bundle.[contenthash].js',
         path: path.resolve(__dirname, '../dist')
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
     },
     devtool: 'source-map',
     plugins:
     [
+        ...pages.map(
+            (page) =>
+                new HtmlWebpackPlugin({
+                    inject: true,
+                    template: `./src/${page}.html`,
+                    filename: `${page}.html`,
+                    chunks: [page],
+                })
+        ),
         new CopyWebpackPlugin({
             patterns: [
                 { from: path.resolve(__dirname, '../static') }
             ]
         }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
-            minify: true
-        }),
+        // new HtmlWebpackPlugin({
+        //     template: path.resolve(__dirname, '../src/index.html'),
+        //     minify: true
+        // }),
         new MiniCSSExtractPlugin()
     ],
     module:
@@ -82,7 +100,24 @@ module.exports = {
                         }
                     }
                 ]
-            }
+            },
+
+            // Markdown
+            {
+                test: /\.md$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                    },
+                    {
+                        loader: "markdown-loader",
+                        options: {
+                            // Pass options to marked
+                            // See https://marked.js.org/using_advanced#options
+                        },
+                    },
+                ],
+            },
         ]
     }
 }
